c2e00c1b144f367513868c4705f0993e24fc306b
Adam Ross <14985050+R055A@users.noreply.github.com>
2025-11-13 09:47:51 +0100

    Add repo icon for archived repositories (#76)

--- gh_profile_repo_pins/repo_pins_img/repo_pins_img_svg.py
+++ gh_profile_repo_pins/repo_pins_img/repo_pins_img_svg.py
@@ -47,6 +47,13 @@ class RepoPinImg:
         "M9 10.168V9a3 3 0 1 1 6 0v1.168c.591.281 1 .884 1 1.582v2.5A1.75 1.75 0 0 1 14.25 16h-4.5A1.75 1.75 0 0 1 8 "
         "14.25v-2.5c0-.698.409-1.3 1-1.582ZM13.5 10V9a1.5 1.5 0 0 0-3 0v1Z"
     )
+    __ICON_REPO_ARCHIVE: str = (
+        "M0 2.75C0 1.784.784 1 1.75 1h12.5c.966 0 1.75.784 1.75 1.75v1.5A1.75 1.75 0 0 1 14.25 6H1.75A1.75 1.75 0 0 1 "
+        "0 4.25ZM1.75 7a.75.75 0 0 1 .75.75v5.5c0 .138.112.25.25.25h10.5a.25.25 0 0 0 .25-.25v-5.5a.75.75 0 0 1 1.5 "
+        "0v5.5A1.75 1.75 0 0 1 13.25 15H2.75A1.75 1.75 0 0 1 1 13.25v-5.5A.75.75 0 0 1 1.75 7Zm0-4.5a.25.25 0 0 "
+        "0-.25.25v1.5c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25v-1.5a.25.25 0 0 0-.25-.25ZM6.25 8h3.5a.75.75 0 0 "
+        "1 0 1.5h-3.5a.75.75 0 0 1 0-1.5Z"
+    )
     __ICON_STAR: str = (
         "M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 "
         "4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 "
@@ -763,9 +770,13 @@ class RepoPinImg:
             if self.__repo_pin_data.is_private
             else [
                 (
-                    self.__ICON_REPO_TEMPLATE
-                    if self.__repo_pin_data.is_template
-                    else self.__ICON_REPO
+                    self.__ICON_REPO_ARCHIVE
+                    if self.__repo_pin_data.is_archived
+                    else (
+                        self.__ICON_REPO_TEMPLATE
+                        if self.__repo_pin_data.is_template
+                        else self.__ICON_REPO
+                    )
                 )
             ]
         )
